f5363e1cd6646662c0f669bbd983e6ce5e82b2e2
oladele1440 <oladelelawrence@gmail.com>
2018-02-10 10:32:57 -0600

    adding class files / hw2

---
+++ Pythonstuff/class1.py
@@ -0,0 +1,18 @@
+
+   tittle = "Frankfurter"
+years = 80
+trueOrfalse = False
+print("Ahmed is a prfessional" +"1")
+print("he has been coding for " +str(years))
+print("pretty muall all of that was " _+str(trueOfFalse)
+print(years)tittle = "Frankfurter"
+years = 80
+trueOrfalse = False
+print("Ahmed is a prfessional" +"1")
+print("he has been coding for " +str(years))
+print("pretty muall all of that was " _+str(trueOfFalse)
+print(years)'
+
+
+
+
